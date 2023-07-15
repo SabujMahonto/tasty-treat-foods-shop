d336d136c61ee465f956060cd0b8ff3f7ed3af2c
sabujmahonto <sabujmahonto@gmail.com>
2023-07-15 13:12:00 +0600

    categoryimages data added

--- app/(Home)/page.tsx
+++ app/(Home)/page.tsx
@@ -1,7 +1,11 @@
+import Category from "@/components/Category";
 import Hero from "@/components/Hero";
 
 export default function Homepage() {
-  return <main>
-    <Hero/>
-  </main>;
+  return (
+    <main>
+      <Hero />
+      <Category />
+    </main>
+  );
 }
---
+++ components/Category.tsx
@@ -0,0 +1,37 @@
+"use client";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// import required modules
+import { Pagination } from "swiper/modules";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+export default function Category() {
+  return (
+    <section className="py-20">
+      {/* {category goes here} */}
+      <div className="w-full h-[50vh]">
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+        </Swiper>
+      </div>
+    </section>
+  );
+}
