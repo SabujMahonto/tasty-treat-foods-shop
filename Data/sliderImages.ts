585e9766ed5cd5715b061edb4e4a64ced1465372
sabujmahonto <sabujmahonto@gmail.com>
2023-07-15 12:44:11 +0600

    hero section done

---
+++ Data/sliderImages.ts
@@ -0,0 +1,56 @@
+export const data = [
+  {
+    id: 1,
+    src: "https://images.pexels.com/photos/1199960/pexels-photo-1199960.jpeg?auto=compress&cs=tinysrgb&w=600",
+    alt: "Selective Focus Photo of Cheeseburger",
+    headline: "Delicious Cheeseburger",
+    paragraph:
+      "Savor the taste of our mouthwatering cheeseburger made with premium ingredients.",
+    cta: "Order Now",
+  },
+  {
+    id: 2,
+    src: "https://images.pexels.com/photos/4773769/pexels-photo-4773769.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Person Holding a Pizza",
+    headline: "Tasty Pizza",
+    paragraph:
+      "Indulge in our delectable pizza loaded with your favorite toppings and oozy cheese.",
+    cta: "Get Pizza",
+  },
+  {
+    id: 3,
+    src: "https://images.pexels.com/photos/803963/pexels-photo-803963.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Cooked Pasta on Brown Wooden Bowl",
+    headline: "Classic Pasta Dish",
+    paragraph:
+      "Experience the authentic flavors of our homemade pasta cooked to perfection.",
+    cta: "try Now",
+  },
+  {
+    id: 4,
+    src: "https://images.pexels.com/photos/858501/pexels-photo-858501.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Person Eating Sushi",
+    headline: "Fresh Sushi Platter",
+    paragraph:
+      "Enjoy a delightful assortment of freshly prepared sushi rolls with the finest ingredients.",
+    cta: "Order Sushi",
+  },
+  {
+    id: 5,
+    src: "https://images.pexels.com/photos/3690/food-restaurant-hand-dinner.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Person Holding Silver Fork",
+    headline: "Juicy Steak",
+    paragraph:
+      "Treat yourself to a perfectly grilled, tender steak seasoned to perfection.",
+    cta: "Get Steak Now",
+  },
+  {
+    id: 6,
+    src: "https://images.pexels.com/photos/1752506/pexels-photo-1752506.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Close-Up Photo of Ice Cream On Plate",
+    headline: "Creamy Ice Cream",
+    paragraph:
+      "Indulge in our creamy and flavorful ice cream, available in a variety of tempting flavors.",
+    cta: "Order Ice Cream",
+  },
+];
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,69 @@
+"use client";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// import required modules
+import { Autoplay, Pagination, Navigation, Parallax } from "swiper/modules";
+import Image from "next/image";
+import { data } from "@/Data/sliderImages";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import Link from "next/link";
+
 export default function Hero() {
-  return <section>Hero</section>;
+  return (
+    <section className="w-full h-[75vh]">
+      <Swiper
+        parallax={true}
+        loop={true}
+        speed={1000}
+        grabCursor={true}
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation, Parallax]}
+        className="mySwiper w-full h-full"
+      >
+        {data.map((item) => (
+          <SwiperSlide key={item.id}>
+            <div className="w-full h-full relative">
+              <Image
+                src={item.src}
+                alt={item.alt}
+                height={1920}
+                width={1080}
+                priority
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div className="absolute top-1/2 left-1/2 text-center  text-white -translate-x-1/2 -translate-y-1/2 z-[2] space-y-5">
+              <h1
+                className="text-8xl font-semibold"
+                data-swiper-parallax="-300%"
+              >
+                {item.headline}
+              </h1>
+              <p className="text-lg font-medium" data-swiper-parallax="-500%">
+                {item.paragraph}
+              </p>
+              <div data-swiper-parallax="-900%">
+                <Link href={"/product"} className="btn btn-accent">
+                  {item.cta}
+                </Link>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  );
 }
